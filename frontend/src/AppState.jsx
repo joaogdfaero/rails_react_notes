11dd5ccb80c816add277c06b07b042d41130509c
João Gabriel Dal Forno <joao.dal@acad.ufsm.br>
2023-12-04 19:20:48 -0300

    Hello word in React

--- frontend/src/AppState.jsx
+++ frontend/src/AppState.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, {useContext, useReducer} from "react"
 
 // INITIAL STATE
 const initialState = {
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -4,7 +4,7 @@ import "./styles.css";
 import {App} from "./components/App"
 import { createRoot } from "react-dom/client";
 import {BrowserRouter as Router} from "react-router-dom"
-import {AppState} from "./AppState"
+import {AppState} from "./AppState.jsx"
 
 const root = createRoot(document.querySelector("#root"));
 root.render(<AppState><Router><App/></Router></AppState>)
