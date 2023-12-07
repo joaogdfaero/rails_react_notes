3b5a101934c5a9bd55dfe9bc52b9b86dd834c373
João Gabriel Dal Forno <joao.dal@acad.ufsm.br>
2023-12-07 20:33:09 -0300

    login won't working yet

--- frontend/src/pages/Auth.jsx
+++ frontend/src/pages/Auth.jsx
@@ -1,7 +1,15 @@
 import React from "react"
 
 const Auth = (props) => {
-    return <h1>Auth</h1>
+    const type = props.match.params.form;
+
+    return <div>
+        <form>
+            <input type="text" name="username" value=""/>
+            <input type="password" name="password" value=""/>
+            <input type="submit" value={type}/>
+        </form>
+    </div>
 }
 
 export default Auth
