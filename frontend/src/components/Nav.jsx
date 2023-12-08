37ac436bdb2fc125145edba2427251554d2ff5ca
João Gabriel Dal Forno <joao.dal@acad.ufsm.br>
2023-12-08 07:38:48 -0300

    buttons auth events handlers

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -6,8 +6,8 @@ const Nav = (props) => {
         <h1>Faça suas anotações!</h1>
         <nav>
             <Link to="/"><div>Home</div></Link>
-            <Link to="/auth/signup"><div>Home</div></Link>
-            <Link to="/auth/login"><div>Home</div></Link>
+            <Link to="/auth/signup"><div>Signup</div></Link>
+            <Link to="/auth/login"><div>Login</div></Link>
         </nav>
     </header>
 }
--- frontend/src/pages/Auth.jsx
+++ frontend/src/pages/Auth.jsx
@@ -1,15 +1,49 @@
-import React from "react"
+import React from "react";
+import {useAppState} from "../AppState.jsx"
 
 const Auth = (props) => {
-    const type = props.match.params.form;
+  // Destructure match from props
+  const { match } = props;
 
-    return <div>
-        <form>
-            <input type="text" name="username" value=""/>
-            <input type="password" name="password" value=""/>
-            <input type="submit" value={type}/>
-        </form>
+  // Check if match and match.params exist
+  const type = match && match.params ? match.params.form : 'enter'; /* parece que o props ou algum elemento que descende dele está indefinido */
+  const [formData, setFormData] = React.useState({
+    username: "",
+    password: ""
+  })
+
+  const {dispatch} = useAppState()
+
+  const actions = {
+    signup: {
+        action: "signup",
+        payload: formData 
+    },
+    login: {
+        action: "login",
+        payload: formData 
+    }
+  };
+
+  const handleChange = (event) => {
+    setFormData({...formData, [event.target.name] : event.target.value})
+  }; 
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    dispatch(actions[type])
+  } 
+
+  return (
+    <div>
+      <form>
+        <input type="text" name="username" value={formData.username} onChange={handleChange} />
+        <input type="password" name="password" value={formData.password} onChange={handleChange} />
+        <input type="submit" value={type} />
+      </form>
     </div>
-}
+  );
+};
 
-export default Auth
+export default Auth;
+
