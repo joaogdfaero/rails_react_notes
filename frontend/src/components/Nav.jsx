febd0ad92ab891774cf13552d21db843899907f1
João Gabriel Dal Forno <joao.dal@acad.ufsm.br>
2023-12-19 08:30:12 -0300

    logout redirect to home

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,9 +1,12 @@
 import React from "react"
 import {Link} from "react-router-dom"
 import { useAppState } from "../AppState.jsx";
+import { useNavigate } from 'react-router-dom';
 
 const Nav = (props) => {
     const{dispatch} = useAppState()
+
+    const navigate = useNavigate();
 
     return <header>
         <h1>Faça suas anotações!</h1>
@@ -13,7 +16,7 @@ const Nav = (props) => {
             <Link to="/auth/login"><div>Login</div></Link>
             <div onClick={() => {
                 dispatch({type: "logout"})
-                
+                navigate("/");
             }}>Logout</div> 
         </nav>
     </header>
